a678787c1e17be40b937977c6a907dea63c821bc
Jason Shao <shaoz3@rose-hulman.edu>
2023-02-14 16:24:46 -0500

    Fixed login page and now able to function like a charm

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,9 @@
 var rhit = rhit || {};
+rhit.fbAuthManager = null;
+rhit.fbUserManager = null;
+rhit.FB_COLLECTION_USERS = "Users";
+rhit.FB_KEY_NAME = "name";
+rhit.FB_KEY_PHOTO_URL = "photoUrl";
 
 import {
 	initializeGalleryPage,
@@ -8,9 +13,242 @@ import {
 // 	const heroesJson = await getHeroNamesAndImages();
 // }
 
+rhit.SideNavController = class {
+	constructor() {
+		const menuProfilePageItem = document.querySelector("#menuGoToProfilePage");
+		if (menuProfilePageItem) {
+			menuProfilePageItem.addEventListener("click", (event) => {
+				window.location.href = "/profile.html";
+			});
+		}
+		const menuSignOutItem = document.querySelector("#menuSignOut");
+		if (menuSignOutItem) {
+			menuSignOutItem.addEventListener("click", (event) => {
+				rhit.fbAuthManager.signOut();
+			});
+		}
+	}
+}
+
+rhit.FbAuthManager = class {
+	constructor() {
+		this._user = null;
+		this._name = "";
+		this._photoUrl = "";
+	}
+
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			changeListener();
+		});
+	}
+
+	signIn() {
+		console.log("Sign in");
+		Rosefire.signIn("e1e20514-f4a6-453b-824d-2d0e022d3776", (err, rfUser) => {
+			if (err) {
+				console.log("Rosefire error!", err);
+				return;
+			}
+			console.log("Rosefire success!", rfUser);
+			this._name = rfUser.name;
+			firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				if (errorCode == 'auth/invalid-custom-token') {
+					alert('The token you provided is not valid');
+				} else {
+					console.error("Custom auth error", errorCode, errorMessage);
+				}
+			});
+		});
+	}
+
+	signOut() {
+		firebase.auth().signOut().catch((error) => {
+			console.log("Error");
+		});
+	}
+
+	startFirebaseUI = function () {
+
+		var uiConfig = {
+			signInSuccessUrl: '/',
+			signInOptions: [
+				firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+				firebase.auth.EmailAuthProvider.PROVIDER_ID,
+				firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+				firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+			],
+		};
+
+		const ui = new firebaseui.auth.AuthUI(firebase.auth());
+		ui.start('#firebaseui-auth-container', uiConfig);
+	}
+
+	get isSignedIn() {
+		return !!this._user;
+	}
+
+	get uid() {
+		return this._user.uid;
+	}
+
+	get name() {
+		return this._name || this._user.displayName;
+	}
+
+	get photoUrl() {
+		return this._photoUrl || this._user.photoURL;
+	}
+}
+
+rhit.FbUserManager = class {
+	constructor() {
+		this._collectoinRef = firebase.firestore().collection(rhit.FB_COLLECTION_USERS);
+		this._document = null;
+	}
+
+	addNewUserMaybe(uid, name, photoUrl) {
+		const userRef = this._collectoinRef.doc(uid);
+		return userRef.get().then((doc) => {
+			if (doc.exists) {
+				console.log("User Already Exist: ", doc.data());
+				return false;
+			} else {
+				console.log("Create this user!");
+				return userRef.set({
+					[rhit.FB_KEY_NAME]: name,
+					[rhit.FB_KEY_PHOTO_URL]: photoUrl,
+				}).then(function () {
+					console.log("Successfully Written!");
+					return true;
+				}).catch(function (error) {
+					console.error("Error writing  doc:", error);
+				});
+			}
+		}).catch(function (error) {
+			console.log("Error getting document: ", error);
+		});
+	}
+
+	beginListening(uid, changeListener) {
+		const userRef = this._collectoinRef.doc(uid);
+		this._unsubscribe = userRef.onSnapshot((doc) => {
+			if (doc.exists) {
+				console.log("Document data:", doc.data());
+				this._document = doc;
+				changeListener();
+			} else {
+				console.log("No User!");
+			}
+		});
+	}
+
+	stopListening() {
+		this._unsubscribe();
+	}
+
+	get isListening() {
+		return !!this._unsubscribe;
+	}
+
+	updatePhotoUrl(photoUrl) {
+		const userRef = this._collectoinRef.doc(rhit.fbAuthManager.uid);
+		userRef.update({
+			[rhit.FB_KEY_PHOTO_URL]: photoUrl,
+		}).then(() => {
+			console.log("Document successfully updated!");
+		}).catch(function (error) {
+			console.error("Error updating document: ", error);
+		});
+	}
+
+	updateName(name) {
+		const userRef = this._collectoinRef.doc(rhit.fbAuthManager.uid);
+		return userRef.update({
+			[rhit.FB_KEY_NAME]: name,
+		}).then(() => {
+			console.log("Document successfully updated!");
+		}).catch(function (error) {
+			console.error("Error updating document: ", error);
+		});
+	}
+
+	get name() { return this._document.get(rhit.FB_KEY_NAME); }
+
+	get photoUrl() { return this._document.get(rhit.FB_KEY_PHOTO_URL); }
+};
+
+rhit.LoginPageController = class {
+	constructor() {
+		document.querySelector("#rosefireButton").onclick = (event) => {
+			rhit.fbAuthManager.signIn();
+		};
+		rhit.fbAuthManager.startFirebaseUI();
+	}
+}
+
+rhit.checkForRedirects = function () {
+	if (document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/homeScreen.html";
+	}
+	if (!document.querySelector("#loginPage") && !rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/";
+	}
+};
+
+rhit.initializePage = function () {
+	if (document.querySelector("#loginPage")) {
+		console.log("You are on the login page.");
+		new rhit.LoginPageController();
+	}
+	if (document.querySelector("#mainPage")) {
+		console.log("You are on the gallery page.");
+		initializeGalleryPage();
+	}
+};
+
+rhit.createUserObjectIfNeeded = function () {
+	return new Promise((resolve, reject) => {
+		//check if a User might be new
+		if (!rhit.fbAuthManager.isSignedIn) {
+			resolve(false);
+			return;
+		}
+		if (!document.querySelector("#loginPage")) {
+			resolve(false);
+			return;
+		}
+		//Call addNewUserMaybe
+		rhit.fbUserManager.addNewUserMaybe(
+			rhit.fbAuthManager.uid,
+			rhit.fbAuthManager.name,
+			rhit.fbAuthManager.photoUrl,
+		).then((isUserNew) => {
+			resolve(isUserNew);
+		});
+	});
+}
+
 rhit.main = function () {
 	console.log("Ready");
-	initializeGalleryPage();
+	rhit.fbAuthManager = new rhit.FbAuthManager();
+	rhit.fbUserManager = new rhit.FbUserManager();
+	rhit.fbAuthManager.beginListening(() => {
+		console.log("Is signed In: ", rhit.fbAuthManager.isSignedIn);
+		rhit.createUserObjectIfNeeded().then((isUserNew) => {
+			if (isUserNew) {
+				window.location.href = "/profile.html";
+				return;
+			}
+			rhit.checkForRedirects();
+			new rhit.SideNavController();
+			rhit.initializePage();
+		});
+	});
+
 	//this.getHeroData();
 };
 //console.log("js working");
